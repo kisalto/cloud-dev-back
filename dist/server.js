81fa5159cdef9019e0d91f29d33d9cc7dec2d67b
kisalto <joshuabinotto@hotmail.com>
2026-08-14 23:10:15 -0300

    Fix: Arruma o tsconfig.json

---
+++ dist/server.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import cors from 'cors';
+import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+const app = express();
+const connectionString = process.env.DATABASE_URL;
+if (!connectionString) {
+    throw new Error('DATABASE_URL não definida');
+}
+const adapter = new PrismaPg({
+    connectionString,
+});
+const prisma = new PrismaClient({
+    adapter,
+});
+app.use(cors());
+app.use(express.json());
+app.get('/', (req, res) => {
+    res.status(200).json({
+        message: 'Reserva de Salas API está rodando perfeitamente! 🚀',
+    });
+});
+const PORT = Number(process.env.PORT) || 8080;
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
